f804658ac257d2fe3d02138fc6c13e9bdd3f82b6
jdefi <nobtx.io@gmail.com>
2021-12-01 01:36:08 +0200

    update gitignore

---
+++ src/views/Claim/Claim.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Route, Switch, useRouteMatch } from 'react-router-dom'
+import { useWallet } from 'use-wallet'
+
+import brt from '../../assets/img/brt.png'
+
+import Button from '../../components/Button'
+import Page from '../../components/Page'
+import PageHeader from '../../components/PageHeader'
+import WalletProviderModal from '../../components/WalletProviderModal'
+
+import useModal from '../../hooks/useModal'
+
+import Farm from '../Farm'
+
+import FarmCards from './components/FarmCards'
+
+const Claim: React.FC = () => {
+  const { path } = useRouteMatch()
+  const { account } = useWallet()
+  const [onPresentWalletProviderModal] = useModal(<WalletProviderModal />)
+  return (
+    <Switch>
+      <Page>
+        {!!account ? (
+          <>
+            <Route exact path={path}>
+              <PageHeader
+                icon={<img src={brt} height="120" />}
+                subtitle="Earn HopeNobt tokens by referring your friends."
+                title="Claim Your HopeNobt Airdrop"
+              />
+              <FarmCards />
+            </Route>
+            <Route path={`${path}/:farmId`}>
+              <Farm />
+            </Route>
+          </>
+        ) : (
+          <div
+            style={{
+              alignItems: 'center',
+              display: 'flex',
+              flex: 1,
+              justifyContent: 'center',
+            }}
+          >
+            <Button
+              onClick={onPresentWalletProviderModal}
+              text="🔓 Unlock Wallet"
+            />
+          </div>
+        )}
+      </Page>
+    </Switch>
+  )
+}
+
+export default Claim
